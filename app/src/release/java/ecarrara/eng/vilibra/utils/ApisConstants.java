b0eeb0173f569de9912475d154ba71366f0f553b
Eduardo Carrara <ecarrara.araujo@gmail.com>
2015-09-16 11:49:23 -0300

    Removing unnecessary release version of ApisConstants class.
    Signed-off-by: Eduardo Carrara <ecarrara.araujo@gmail.com>

--- app/src/release/java/ecarrara/eng/vilibra/utils/ApisConstants.java
+++
@@ -1,8 +0,0 @@
-package ecarrara.eng.vilibra.utils;
-
-/**
- * Created by ecarrara on 12/10/14.
- */
-public class ApisConstants {
-    public static final String GOOGLE_BOOKS_API_KEY = "AIzaSyDpL_Gbscclt_YWy_8LEA_hjVJ6dmy8WHY";
-}
